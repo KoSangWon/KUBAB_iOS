3730615c09c8869e8cfd46fcd725018c948e341d
KoSangWon <jackoss@naver.com>
2020-02-05 17:18:01 +0900

    first commit

---
+++ KUBAB_iOS/KUBAB_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KUBAB_iOS
+//
+//  Created by 고상원 on 2020/02/05.
+//  Copyright © 2020 고상원. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
